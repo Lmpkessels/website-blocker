835b4db8555f7d3237c0b5db12400434ae7e5454
lkessels <l@lmpkessels.com>
2026-03-16 11:49:39 +0100

    add: constant with /etc/hosts.stored to store domains

--- src/constants.rs
+++ src/constants.rs
@@ -1,1 +1,5 @@
 pub const HOSTS_PATH: &str = "/etc/hosts"; // The path to write domains towards
+
+pub const STORED_DOMAINS: &str = "/etc/hosts.stored"; // The path that stores the 
+// domain till the block is started
+
